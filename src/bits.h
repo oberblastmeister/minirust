db45a9a4f235b4a147c7c1b9bacce76e12221d1f
Brian Shu <littlebubu.shu@gmail.com>
2022-07-04 15:31:21 -0400

    hash stuff

--- src/bits.h
+++ src/bits.h
@@ -24,28 +24,46 @@ static inline uint64_t next_power_of_2(uint64_t i) {
 
 // see
 // https://stackoverflow.com/questions/776508/best-practices-for-circular-shift-rotate-operations-in-c
-#define MAKE_ROT(T, NAME)                                                      \
-    static inline T rotl##NAME(T n, uint32_t c) {                              \
+#define MAKE_ROT(T)                                                            \
+    static inline T rotl_##T(T n, uint32_t c) {                                \
         const uint32_t mask = (CHAR_BIT * sizeof(n) - 1);                      \
         c &= mask;                                                             \
         return (n << c) | (n >> ((-c) & mask));                                \
     }                                                                          \
-    static inline T rotr##NAME(T n, uint32_t c) {                              \
+    static inline T rotr_##T(T n, uint32_t c) {                                \
         const uint32_t mask = (CHAR_BIT * sizeof(n) - 1);                      \
         c &= mask;                                                             \
         return (n >> c) | (n << ((-c) & mask));                                \
     }
 
-MAKE_ROT(uint8_t, 8)
-MAKE_ROT(uint16_t, 16)
-MAKE_ROT(uint32_t, 32)
-MAKE_ROT(uint64_t, 64)
+MAKE_ROT(size_t)
+MAKE_ROT(uint8_t)
+MAKE_ROT(uint16_t)
+MAKE_ROT(uint32_t)
+MAKE_ROT(uint64_t)
 
 #undef MAKE_ROT
-    
-// #define rotl(n, c) \
-//     _Generic((n), \
-//         uint8_t: rotl8((n), (s))
-//     )
+
+#define rotl(n, c)                                                             \
+    ({                                                                         \
+        typeof(n) _n = (n);                                                    \
+        typeof(c) _c = (c);                                                    \
+        _Generic((_n), uint8_t                                                 \
+                 : rotl8(_n, _c), uint16_t                                     \
+                 : rotl16(_n, _c), uint32_t                                    \
+                 : rotl32(_n, _c), uint64_t                                    \
+                 : rotl64(_n, _c));                                            \
+    })
+
+#define rotr(n, c)                                                             \
+    ({                                                                         \
+        typeof(n) _n = (n);                                                    \
+        typeof(c) _c = (c);                                                    \
+        _Generic((n), uint8_t                                                  \
+                 : rotr8(_n, _c), uint16_t                                     \
+                 : rotr16(_n, _c), uint32_t                                    \
+                 : rotr32(_n, _c), uint64_t                                    \
+                 : rotr64(_n, _c));                                            \
+    })
 
 #endif
--- src/hash.h
+++ src/hash.h
@@ -1,20 +1,65 @@
+// see https://github.com/rust-lang/rustc-hash
+
 #ifndef _LOX_HASH_H
 #define _LOX_HASH_H
 
+#include "bits.h"
 #include "prelude.h"
+#include "read_bytes.h"
+#include <assert.h>
+#include <string.h>
 
-#if __SIZEOF_SIZE_T__ == 8
-#define FX_CONST 0x517cc1b727220a95
-#elif __SIZEOF_SIZE_T__ == 4
-#define FX_CONST 0x9e3779b9
-#else
-#error "size_t must be either 8 or 4 bytes for hash.h to work"
-#endif
+typedef struct {
+    size_t hash;
+} hasher;
 
-typedef size_t hash;
+typedef uint64_t hash;
 
-static inline add_size_t(hash h, size_t i) {
-
+static inline void hash_size_t(hasher *h, size_t i) {
+    // exploits wrapping multiplication of FX_CONST
+    // size_t is unsigned, and will always wrap when overflowed
+    // https://stackoverflow.com/questions/18195715/why-is-unsigned-integer-overflow-defined-behavior-but-signed-integer-overflow-is
+    // hopefully the compiler will optimize this
+    if (sizeof(size_t) == 8) {
+        h->hash = (rotl_size_t(h->hash, 5) ^ i) * 0x517cc1b727220a95;
+    } else {
+        h->hash = (rotl_size_t(h->hash, 5) ^ i) * 0x9e3779b9;
+    }
 }
+
+#define _MAKE_INT_HASHER(T)                                                    \
+    static inline void hash_##T(hasher *h, T i) { hash_size_t(h, (size_t)i); }
+
+_MAKE_INT_HASHER(uint8_t)
+_MAKE_INT_HASHER(uint16_t)
+_MAKE_INT_HASHER(uint32_t)
+_MAKE_INT_HASHER(uint64_t)
+
+#undef INT_HASHER
+
+static inline void hash_bytes(hasher *h, uint8_t *bytes, size_t len) {
+    while (len >= sizeof(size_t)) {
+        hash_size_t(h, read_size_t(bytes));
+        bytes += sizeof(size_t);
+        len -= sizeof(size_t);
+    }
+    if ((sizeof(size_t) > 4) && len >= 4) {
+        hash_size_t(h, (size_t)read_uint32_t(bytes));
+        bytes += sizeof(uint32_t);
+        len -= sizeof(uint32_t);
+    }
+    if ((sizeof(size_t) > 2) && len >= 2) {
+        hash_size_t(h, (size_t)read_uint16_t(bytes));
+        bytes += sizeof(uint16_t);
+        len -= sizeof(uint16_t);
+    }
+    if ((sizeof(size_t) > 1) && len >= 1) {
+        hash_size_t(h, (size_t)bytes[0]);
+    }
+}
+
+static inline void hash_bool(hasher *h, bool b) { hash_uint8_t(h, (uint8_t)b); }
+
+static inline hash hasher_finish(hasher h) { return (uint64_t)h.hash; }
 
 #endif
